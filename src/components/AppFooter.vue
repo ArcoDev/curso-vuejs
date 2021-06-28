6d6f37daea9cb9af6a2f7f8e2e211c6543a0ab02
arcodev <chris_arco0794@hotmail.com>
2021-06-28 16:41:37 -0500

    Primeros pasos con componentes

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+        <span>💻 witch 💚 <a href="https://vuejs.org">Vue Js</a> in arcodev</span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'app-footer'
+}
+</script>
+
+<style lang="scss" scoped>
+    footer {
+        box-sizing: border-box;
+        background-color: #cfd8dc;
+        bottom: 0;
+        left: 0;
+        padding: 1rem;
+        position: absolute;
+        width: 100%;
+    }
+</style>
